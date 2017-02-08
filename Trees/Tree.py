8126a675404f8440815a13e6e1d47ae27d0ac8a8
midhunsezhi <midhunsezhi@gmail.com>
2017-02-07 16:27:44 -0800

    implement abstract base class for tree

---
+++ Trees/Tree.py
@@ -0,0 +1,55 @@
+"""
+Abstract base class for Tree implementation
+"""
+
+class Tree:
+	"""
+	abstract position class
+	"""
+	class Position:
+		def element(self):
+			""" return the element stored at this position """
+			raise NotImplementedError('must be implemented by subclass')
+
+		def __eq__(self, other):
+			"""return true if other position represents the same location"""
+			raise NotImplementedError('must be implemented by subclass')
+
+		def __nq__(self, other):
+			return not (self == other) #this calls the previous method
+
+
+	"""abstract methods"""
+
+	def root(self):
+		"""return the position representing tree's root"""
+		raise NotImplementedError('must be implemented by subclass')
+
+	def parent(self, p):
+		""" return position representing p's parent, return None if root"""
+		raise NotImplementedError('must be implemented by subclass')
+
+	def num_children(self, p):
+		"""return number of children that position p has"""
+		raise NotImplementedError('must be implemented by subclass')
+
+	def childern(self, p):
+		"""return the iterations of positions representing p's children"""
+		raise NotImplementedError('must be implemented by subclass')
+
+	def __len__(self):
+		"""return total number of elements in the tree"""
+		raise NotImplementedError('must be implemented by subclass')
+
+	""" concrete methods """
+
+	def is_root(self, p):
+		return self.root() == p
+
+	def is_leaf(self, p):
+		return self.num_children(p) == 0
+
+	def is_empty(self):
+		return len(self) == 0
+
+
